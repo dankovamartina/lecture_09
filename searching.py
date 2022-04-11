2844da80746fe4896ce5ce7987fae826efec2d5f
Martina Dankova <andorianka@gmail.com>
2022-04-11 08:54:07 +0200

    fixed read data fucntion

--- searching.py
+++ searching.py
@@ -1,4 +1,7 @@
 import os
+import json
+
+#balicek os resi problemy s lomitky pri zadavani cest - podiva se, co mam za operacni system a podle toho tam vlozi potrebna lomitka
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -11,11 +14,24 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
+    #napred osetrime, jeslti je pozadovany klic field v MNOZINE pozadovanych reseni
+
+    #tady spojujeme dve cesty dohromady - ziskame absolutni cestu k souboru
     file_path = os.path.join(cwd_path, file_name)
+
+    with open (file_path, mode ="r") as json_file:
+        data = json.load(json_file)
+
+ #napred osetrime, jeslti je pozadovany klic field v MNOZINE pozadovanych reseni
+ #klice nahazime do mnoziny prikazem set
+    if field not in set(data.keys):
+        return None
+    return data[field]  # vracime jen kousek toho slovniku podle klice
 
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
 
 
 if __name__ == '__main__':
